81815c41fd1a746b8fc5e42e2d2452178142e6f5
caoye <mailcaoye@163.com>
2017-12-15 18:58:53 +0800

    first commit

--- src/main/java/Cy/WeChatSpringboot/App.java
+++ src/main/java/Cy/WeChatSpringboot/App.java
@@ -1,15 +1,19 @@
 package Cy.WeChatSpringboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
-public class App{
+public class App {
+	@Value("${test.msg}")
+	private String msg;
 
 	@RequestMapping("/")
 	String home() {
+		System.out.println(msg);
 		return "Hello World!";
 	}
 
