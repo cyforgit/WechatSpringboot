c83732d5ec861c3a8b4cf0e366f3feda6e93c3cf
caoye <mailcaoye@163.com>
2017-12-15 18:52:42 +0800

    init

---
+++ src/main/java/Cy/WeChatSpringboot/App.java
@@ -0,0 +1,20 @@
+package Cy.WeChatSpringboot;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class App{
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
+}
