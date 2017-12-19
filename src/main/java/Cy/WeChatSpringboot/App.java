95849802434f364ea2e368b7ac0d27fe9fd9e502
caoye <mailcaoye@163.com>
2017-12-19 18:07:23 +0800

    thymeleaf unfinished

--- src/main/java/Cy/WeChatSpringboot/App.java
+++ src/main/java/Cy/WeChatSpringboot/App.java
@@ -5,24 +5,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import Cy.WeChatSpringboot.dao.usersMapper;
 
 @MapperScan("Cy.WeChatSpringboot.Dao")
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class App {
 	@Autowired
 	private usersMapper users;
-	
+
 	@Value("${test.msg}")
 	private String msg;
-	
+
 	@RequestMapping("/")
 	String home() {
 		System.out.println(users.selectByPrimaryKey(1).toString());
 		return "Hello World!";
+	}
+
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("name", "world");
+
+		return "/hello";
 	}
 
 	public static void main(String[] args) throws Exception {
