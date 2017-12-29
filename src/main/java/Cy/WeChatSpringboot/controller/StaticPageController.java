a6a3dd99800bbafde50b4ddaab9fdb63410c2806
caoye <mailcaoye@163.com>
2017-12-29 10:20:58 +0800

    添加静态页面，前后端分离

---
+++ src/main/java/Cy/WeChatSpringboot/controller/StaticPageController.java
@@ -0,0 +1,18 @@
+package Cy.WeChatSpringboot.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author caoye1 映射静态页面，页面通过restful接口获取并展示数据。
+ */
+@Controller
+@EnableAutoConfiguration
+public class StaticPageController {
+	@RequestMapping("/index")
+	public String getIndex() {
+		return "index";
+	}
+}
